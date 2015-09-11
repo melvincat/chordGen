8b703a34417d1703e4900c84eca9e9fb08db88d4
Melvincat <nichildo@gmail.com>
2015-09-11 18:09:14 +1000

    Initial Commit

---
+++ chordGen/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  chordGen
+//
+//  Created by Nicholas on 11/09/2015.
+//  Copyright (c) 2015 Nicholas. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
