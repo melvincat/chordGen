8d34ef1d8457c60587cf80621b7228970b41eaa3
Melvincat <nichildo@gmail.com>
2015-09-12 10:51:47 +1000

    Added attributes

---
+++ chordGen/attributes.h
@@ -0,0 +1,13 @@
+//
+//  attributes.h
+//  chordGen
+//
+//  Created by Nicholas on 11/09/2015.
+//  Copyright (c) 2015 Nicholas. All rights reserved.
+//
+
+#ifndef chordGen_attributes_h
+#define chordGen_attributes_h
+
+
+#endif
