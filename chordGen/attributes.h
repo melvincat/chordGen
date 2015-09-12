40a58482bf92a3c8132dab5dac60a035f00c633c
Melvincat <nichildo@gmail.com>
2015-09-12 10:56:10 +1000

    setting up

--- chordGen/attributes.h
+++ chordGen/attributes.h
@@ -8,6 +8,13 @@
 
 #ifndef chordGen_attributes_h
 #define chordGen_attributes_h
-
+#include <string>
+struct atrribute{
+    int time_signature_top = 0;
+    int time_signature_bottom = 0;
+    int bar_number = 0;
+    int chord_change = 0;
+    std::string key;
+};
 
 #endif
--- chordGen/main.cpp
+++ chordGen/main.cpp
@@ -8,8 +8,31 @@
 
 #include <iostream>
 
+void welcome()
+{
+    using std::string;
+    string line1 = "***************************************** \n";
+    string line3 = "\t \t \t Chord Generator";
+    
+    std::cout << line1
+    << line1 << "\n"
+    << line3
+    << "\n" << "\n";
+    std::cout << line1 << line1;
+}
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
-    return 0;
+    //create welcome message
+    welcome();
+    /*create a vector, stores time signature, key, number of bars
+     *number of times the chord changes per bar. This is determined by
+     a struct that holds these values*/
+    
+    
+    /*calls a function that gets the attributes for the vector
+     *via a pointer and sets them */
+    
+    /*send the vector to the chord generator
+     *and it uses them to determine the chords that need to be generated
+     returns this in a vector*/
+    
 }
